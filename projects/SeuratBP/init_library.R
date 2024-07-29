60f337989d241f2445640ffe65c30f04290a4394
shuu5 <blackcow.in.aja@gmail.com>
2024-07-29 21:15:10 +0900

    4.4.0に更新

--- projects/SeuratBP/init_library.R
+++ projects/SeuratBP/init_library.R
@@ -1,9 +1,8 @@
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-install.packages("renv")
-
-renv::init()
-
-renv::install("tidyverse")
+# renvをactivate
+renv::restore()
 
 # BPCells をインストール
-install.packages("/home/rstudio/.local/share/renv/cache/bnprks-BPCells-v0.2.0-20-gb78bc83.tar.gz")
+remotes::install_github("bnprks/BPCells/r")
+
+# 環境を保存
+renv::snapshot()
