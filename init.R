8883099c3e35cf75f393abf656be79ce53a57e1f
shuu5 <blackcow.in.aja@gmail.com>
2024-07-31 19:20:51 +0900

    projectsフォルダに複数projectをまとめられるように変更

---
+++ init.R
@@ -0,0 +1,2 @@
+install.packages("remotes")
+remotes::install_github("bnprks/BPCells/r")
--- project/init.R
+++
@@ -1,6 +0,0 @@
-# githubから直接ダウンロードする必要があるものは個別に設定
-renv::install("remotes")
-remotes::install_github("bnprks/BPCells/r")
-remotes::install_github("mojaveazure/seurat-disk")
-
-renv::restore()
